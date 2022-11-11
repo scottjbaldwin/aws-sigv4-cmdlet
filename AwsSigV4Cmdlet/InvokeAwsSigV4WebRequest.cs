0b4ee4b5a12a3cea64c339b16d11c0c03f57cb4b
scott.baldwin <scott.baldwin@purple.telstra.com>
2022-11-11 22:30:08 +1100

    Add inputs and outputs
    Keeping it very simple to start with

--- AwsSigV4Cmdlet/InvokeAwsSigV4WebRequest.cs
+++ AwsSigV4Cmdlet/InvokeAwsSigV4WebRequest.cs
@@ -3,8 +3,32 @@
 namespace AwsSigV4Cmdlet
 {
     [Cmdlet(VerbsLifecycle.Invoke, "AwsSigV4WebRequest")]
+    [OutputType(typeof(HttpResponseMessage))]
     public class InvokeAwsSigV4WebRequest : Cmdlet
     {
-        
+        [Parameter(Mandatory = true, Position = 0)]
+        public Uri Uri { get; set; }
+
+        [Parameter(Mandatory = false, Position = 1)] 
+        public string Body { get; set; }
+
+        [Parameter()]
+        public string AccessKey { get; set; }
+
+        [Parameter()]
+        public string SecretKey { get; set; }
+
+        [Parameter()]
+        public string Token { get; set; }
+
+        [Parameter()]
+        [ValidateSet("GET", "POST", "PUT", "DELETE")]
+        public string Method { get; set; }
+
+        [Parameter()]
+        public string Region { get; set; }
+
+        [Parameter()]
+        public string Service { get; set; }
     }
 }
